2ac09cc052ab55e451033beaac27b6a22e634577
vanilla thunder <marat@marat-bedoev.net>
2013-06-05 09:12:44 +0200

    [UPDATE] logs ui + functions

--- controller/vtdu_logs.php
+++ controller/vtdu_logs.php
@@ -1,134 +1,121 @@
 <?php
 
-class vtdu_logs extends oxAdminView {
+	class vtdu_logs extends oxAdminView
+	{
 
-  protected $_tpl = 'vtdu_logs.tpl';
-  protected $_sExLogPath;
-  protected $_sErrorLog;
-  protected $_sDbLog;
-  protected $_sMailsLog;
-
-  public function init() {
-	parent::init();
-	$cfg = $this->getConfig();
-	$this->_sExLogPath = $cfg->getConfigParam('sShopDir') . 'log/EXCEPTION_LOG.txt';
-	$this->_sErrorLog = ( $cfg->getConfigParam('bErrLog') ) ? $cfg->getConfigParam('sShopDir') . $cfg->getConfigParam('sErrLog') : false;
-	$this->_sDbLog = ( $cfg->getConfigParam('bDbLog') ) ? $cfg->getConfigParam('sShopDir') . $cfg->getConfigParam('sDbLog') : false;
-	$this->_sMailsLog = ( $cfg->getConfigParam('bMailLog') ) ? $cfg->getConfigParam('sShopDir') . $cfg->getConfigParam('sMailLog') : false;
-  }
-
-  public function render() {
-	parent::render();
-	$cfg = $this->getConfig();
+		protected $_tpl = 'vtdu_logs.tpl';
+		protected $_sExLog = null;
+		protected $_sSrvErrLog = null;
+		protected $_sSqlLog = null;
+		protected $_sMailsLog = null;
 
 
-	//var_dump("<h2>".$this->_sExLogPath."</h2>");
-	//$this->getExceptionLog();
-	//echo "<hr/>";
-	//var_dump("<h2>".$this->_sErrorLog."</h2>");
-	//$this->_aViewData['ErrorLog'] = $this->getErrorLog();
-	//echo "<hr/>";
-	//var_dump("<h2>".$this->_sDbLog."</h2>");
-	//echo "<hr/>";
-	//var_dump("<h2>".$this->_sMailLog."</h2>");
-	//$this->_aViewData['ExceltionLog'] = "logs";
-
-
-
-	return $this->_tpl;
-  }
-
-  public function getExceptionLog() {
-	$cfg = $this->getConfig();
-
-
-	$mdaExs = array();
-	$arrX = 0;
-	foreach (file($this->_sExLogPath) as $line) {
-	  if (strpos($line, "--------------------------------------------") !== false) {
-		$arrX++;
-	  } elseif (strlen(trim($line)) != 0) {
-		$mdaExs[$arrX][] = trim($line);
-	  }
+	public function init()
+	{
+		parent::init();
+		$cfg = oxRegistry::getConfig();
+		$this->_sExLog = $cfg->getConfigParam('sShopDir') . 'log/EXCEPTION_LOG.txt';
+		$this->_sSrvErrLog = ( $cfg->getConfigParam('bSrvErrLog')) ? $cfg->getConfigParam('sShopDir') . $cfg->getConfigParam('sSrvErrLog') : false;
+		$this->_sSqlLog    = ( $cfg->getConfigParam('bSqlLog')   ) ? $cfg->getConfigParam('sShopDir') . $cfg->getConfigParam('sSqlLog')    : false;
+		$this->_sMailsLog  = ( $cfg->getConfigParam('bMailLog')  ) ? $cfg->getConfigParam('sShopDir') . $cfg->getConfigParam('sMailLog')   : false;
 	}
+		public function render()
+		{
+			parent::render();
+			$cfg = oxRegistry::getConfig();
 
-	/* echo"<pre>";
-	  foreach ($mdaExs as $aEx) {
-	  $xTime = strpos($aEx[0], "(time:") + 7;
-	  $yTime = strpos($aEx[0], "): [0]");
-	  echo "<p><strong>$xTime -> $yTime</strong></br>";
-	  echo substr($aEx[0], $xTime, $yTime - $xTime) . " " . substr($aEx[0], $yTime + 7) . "<br/>" . end($aEx) . "<br/>";
-	  // getting filename
-	  $sExFile = substr($aEx[2], 3, strpos($aEx[2], "(") - 3);
-	  $iExRow = substr($aEx[2], strpos($aEx[2], "(") + 1, strpos($aEx[2], ")") - strpos($aEx[2], "(") - 1);
-	  echo "Datei: $sExFile<br/>";
-	  echo "Zeile: $iExRow<br/>";
-	  $aExFile = file($sExFile);
-	  echo "<pre>";
-	  echo ($iExRow - 3) . "   " . $aExFile[$iExRow - 3];
-	  echo ($iExRow - 2) . "   " . $aExFile[$iExRow - 2];
-	  echo ($iExRow - 1) . "   <strong style='color:red;'>" . $aExFile[$iExRow - 1] . "</strong>";
-	  echo ($iExRow) . "   " . $aExFile[$iExRow];
-	  echo ($iExRow + 1) . "   " . $aExFile[$iExRow + 1];
-	  echo"</pre>";
-	  echo"</p><hr/>";
-	  }
-	  //var_dump($aContent);
-	  echo"</pre>"; */
-  }
 
-  public function getErrorLog() {
-	if (!$this->_sErrorLog)
-	  return false;
 
-	$aErrorLog = file($this->_sErrorLog);
+			$this->_aViewData['ExLog']     = $this->getExceptionLog();
+			$this->_aViewData['SrvErrLog'] = "ok";
+			$this->_aViewData['SqlLog']    = "ok";
+			$this->_aViewData['MailLog']   = "ok";
 
-	$aParsedLog = array();
-	$x = 0;
-	foreach ($aErrorLog as $row) {
-	  /*
-	    $dateA = strpos($row,"[")+1;
-	    $dateB = strpos($row,"]");
-	    $aParsedLog[$x][] = substr($row, $dateA, $dateB-$dateA);
+			//var_dump("<h2>".$this->_sExLogPath."</h2>");
+			//$this->getExceptionLog();
+			//echo "<hr/>";
+			//var_dump("<h2>".$this->_sErrorLog."</h2>");
+			//$this->_aViewData['ErrorLog'] = $this->getErrorLog();
+			//echo "<hr/>";
+			//var_dump("<h2>".$this->_sDbLog."</h2>");
+			//echo "<hr/>";
+			//var_dump("<h2>".$this->_sMailLog."</h2>");
+			//$this->_aViewData['ExceltionLog'] = "logs";
 
-	    $typeA = strpos($row,"[", $dateA)+1;
-	    $typeB = strpos($row,"]", $typeA);
-	    $aParsedLog[$x][] = substr($row, $typeA, $typeB-$typeA);
 
-	    $errTypeA = strpos($row,"]", $typeB+1)+1;
-	    $errTypeB = strpos($row,":", $errTypeA);
-	    $aParsedLog[$x][] = substr($row, $errTypeA, $errTypeB-$errTypeA);
+			return $this->_tpl;
+		}
 
-	    //$msgA = strpos($row,"]", $errTypeB+1)+1;
-	    $msgB = strpos($row,", referer");
-	    $aParsedLog[$x][] = trim(substr($row, $errTypeB+1, $msgB-$errTypeB-1));
+		public function getExceptionLog()
+		{
+			$cfg = oxRegistry::getConfig();
+			if ( !file_exists($this->_sExLog) || !is_readable($this->_sExLog) )
+			{
+				return "ERROR: file does not exist od is not readable";
+			}
 
-	    $aParsedLog[$x][] = $row;
-	    $x++;
-	   */
-        /*
-	  $regex = '/^\[([^\]]+)\] \[([^\]]+)\] (?:\[client ([^\]]+)\])?\s*(.*)$/i';
-	  $matches = Array();
-	  preg_match($regex, $aErrorLog, $matches);
-	  
-	  $date = $matches[1]; //Date and time
-	  $severity = $matches[2]; // severity
-	  $client = $matches[3]; // client addr (if present)
-	  $msg = $matches[4]; // log message
-	   
-	  $aParsedLog[$date][] = $severity;
-	  $aParsedLog[$date][] = $client;
-	  $aParsedLog[$date][] = $msg;
+			$sData = file_get_contents($this->_sExLog);
+			$aData = explode("---------------------------------------------", $sData);
+			$aData = array_slice($aData, -10, 10);
+
+			return array("ok1","ok2",count($aData));
+		}
+
+		public function getErrorLog()
+		{
+			if (!$this->_sErrorLog)
+				return false;
+
+			$aErrorLog = file($this->_sErrorLog);
+
+			$aParsedLog = array();
+			$x = 0;
+			foreach ($aErrorLog as $row)
+			{
+				/*
+					 $dateA = strpos($row,"[")+1;
+					 $dateB = strpos($row,"]");
+					 $aParsedLog[$x][] = substr($row, $dateA, $dateB-$dateA);
+
+					 $typeA = strpos($row,"[", $dateA)+1;
+					 $typeB = strpos($row,"]", $typeA);
+					 $aParsedLog[$x][] = substr($row, $typeA, $typeB-$typeA);
+
+					 $errTypeA = strpos($row,"]", $typeB+1)+1;
+					 $errTypeB = strpos($row,":", $errTypeA);
+					 $aParsedLog[$x][] = substr($row, $errTypeA, $errTypeB-$errTypeA);
+
+					 //$msgA = strpos($row,"]", $errTypeB+1)+1;
+					 $msgB = strpos($row,", referer");
+					 $aParsedLog[$x][] = trim(substr($row, $errTypeB+1, $msgB-$errTypeB-1));
+
+					 $aParsedLog[$x][] = $row;
+					 $x++;
+					*/
+				/*
+			  $regex = '/^\[([^\]]+)\] \[([^\]]+)\] (?:\[client ([^\]]+)\])?\s*(.*)$/i';
+			  $matches = Array();
+			  preg_match($regex, $aErrorLog, $matches);
+
+			  $date = $matches[1]; //Date and time
+			  $severity = $matches[2]; // severity
+			  $client = $matches[3]; // client addr (if present)
+			  $msg = $matches[4]; // log message
+
+			  $aParsedLog[$date][] = $severity;
+			  $aParsedLog[$date][] = $client;
+			  $aParsedLog[$date][] = $msg;
+			}
+			return $aParsedLog;
+
+
+			echo"<pre>";
+			for ($i = count($aErrorLog) - 10; $i < count($aErrorLog); $i++) {
+			  echo $aErrorLog[$i];
+			}
+			echo "</pre>";
+		  */
+			}
+
+		}
 	}
-	return $aParsedLog;
-
-
-	echo"<pre>";
-	for ($i = count($aErrorLog) - 10; $i < count($aErrorLog); $i++) {
-	  echo $aErrorLog[$i];
-	}
-	echo "</pre>";
-  */
-    }
-
-}
--- views/admin/vtdu_logs.tpl
+++ views/admin/vtdu_logs.tpl
@@ -1,48 +1,61 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-  <head>
-	<title>Category Banner</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta http-equiv="X-UA-Compatible" value="IE=edge" />
-	[{if isset($meta_refresh_sec,$meta_refresh_url) }]
-	  <meta http-equiv=Refresh content="[{$meta_refresh_sec}];URL=[{$meta_refresh_url|replace:"&amp;":"&"}]" />
-	[{/if}]
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>vt DevUtils 2</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="[{$oViewConf->getResourceUrl()}]main.css" />
-	<link rel="stylesheet" href="[{$oViewConf->getResourceUrl()}]colors.css" />
-	<link rel="stylesheet" type="text/css" href="[{$oViewConf->getResourceUrl()}]yui/build/assets/skins/sam/container.css" />
-  </head>
-  <body>
-	<div class="[{$box|default:'box'}]" style="[{if !$box && !$bottom_buttons}]height: 98%;[{/if}]">
-	  <h1>exception log</h1>
-	  <pre>[{$ExceltionLog}]</pre>
+		<link rel="stylesheet" media="screen" href="[{$oViewConf->getModuleUrl('vt-devutils','out/css/bootstrap.min.css') }]">
+		<link rel="stylesheet" media="screen" href="[{$oViewConf->getModuleUrl('vt-devutils','out/css/bootstrap-responsive.min.css') }]">
+		<link rel="stylesheet" media="screen" href="[{$oViewConf->getModuleUrl('vt-devutils','out/css/vt-devutils.css') }]?[{$smarty.now}]">
+	</head>
+	<body>
+		<div class="container fullheight">
+			<div class="row">
+				[{if $ExLog}]
+					<div class="span6 log">
+						<h2>exception log</h2><hr/>
+						[{foreach from=$ExLog item=ex}]
+							[{$ex|var_dump}]<hr/>
+						[{/foreach}]
+					</div>
+				[{/if}]
+				[{if $SrvErrLog}]
+					<div class="span6 log">
+						<h2>webserver error log</h2><hr/>
+						[{$SrvErrLog}]
+					</div>
+				[{/if}]
+			</div>
+			<div class="row">
+				[{if $SqlLog}]
+					<div class="span6 log">
+						<h2>MySQL log</h2><hr/>
+						[{$SqlLog}]
+					</div>
+				[{/if}]
+				[{if $MailLog}]
+					<div class="span6 log">
+						<h2>mail log</h2><hr/>
+						[{$MailLog}]
+					</div>
+				[{/if}]
+			</div>
 
-	  <h1>Webserver Error Log ([{$oView->getErrorLog()|@count}])</h1>
-	  <table border="0" style="border-collapse:collapse">
-		[{foreach from=$oView->getErrorLog() item="error"}]
-		  [{$error|var_dump}]<hr/>
-		  [{*
-		  <tr><td>[{$error.0}]</td><td>[{$error.1}]</td><td>[{$error.2}]</td></tr>
-		  <tr><td colspan="3">[{$error.3}]</td></tr>
-		  <tr><td colspan="3"><hr/></td></tr>
-		  *}]
-		  
-		[{/foreach}]
-	  </table>
-	  <hr/>
-	  <script type="text/javascript">
-		if (typeof jQuery == 'undefined') {
-		  document.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></' + 'script>');
-		}
-	  </script>
-	  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script> [{* jQuery UI for datapickers and mCustomScrollbar *}]
-	  <script type="text/javascript" src="http://cdn.jquerytools.org/1.2.7/all/jquery.tools.min.js"></script> [{* jQuery TOOLS for tooltip *}]
-	  <script type="text/javascript">
-		$(document).ready(function() {
+		</div>
 
-		});
-	  </script> 
+		<script src="[{$oViewConf->getModuleUrl("vt-devutils","out/js/jquery-1.9.1.js")}]"></script>
+		<script src="[{$oViewConf->getModuleUrl("vt-devutils","out/js/bootstrap.min.js")}]"></script>
+		<script src="[{$oViewConf->getModuleUrl("vt-devutils","out/js/jquery.noty.js")}]"></script>
+		<script src="[{$oViewConf->getModuleUrl("vt-devutils","out/js/jquery.noty.center.js")}]"></script>
+		<script src="[{$oViewConf->getModuleUrl("vt-devutils","out/js/jquery.noty.default.js")}]"></script>
+		<script type="text/javascript">
+			$(document).ready(function () {
+
+			});
+		</script>
 
 
-	</div>
-	[{include file="bottomitem.tpl"}]
+	</body>
+</html>
