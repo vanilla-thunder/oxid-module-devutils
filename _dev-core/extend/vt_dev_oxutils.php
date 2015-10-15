fd83ac74c3b0f2482a3dfeb0fd0fe43a46ccfe2a
Marat Bedoev <vanilla-thunder@users.noreply.github.com>
2015-10-15 17:29:14 +0200

    Update vt_dev_oxutils.php

--- _dev-core/extend/vt_dev_oxutils.php
+++ _dev-core/extend/vt_dev_oxutils.php
@@ -3,7 +3,8 @@ class vt_dev_oxutils extends vt_dev_oxutils_parent
 {
     public function setLangCache($sCacheName, $aLangCache)
     {
-        return true;
+        if(oxRegistry::getConfig()->getConfigParam("bl_VtDev_disableLangCache")) return true;
+        return parent::setLangCache($sCacheName, $aLangCache);
     }
     
 }
