680ccec43d92312cda1f290bada0cfda74a0a4aa
Marat <m@marat.ws>
2016-09-01 00:51:01 +0200

    local swf for ng-clip configured, dev-logs usability improvements

--- _dev-core/application/views/admin/vt_dev_footer.tpl
+++ _dev-core/application/views/admin/vt_dev_footer.tpl
@@ -1,7 +1,7 @@
 [{if $b2t != false}]
-<div id="backtotop">
-   <button class="btn btn--xl btn--blue btn--fab" lx-ripple ng-click="scrolltotop()"><i class="mdi mdi-chevron-double-up"></i></button>
-</div>
+    <div id="backtotop">
+        <button class="btn btn--xl btn--blue btn--fab" lx-ripple ng-click="scrolltotop()"><i class="mdi mdi-chevron-double-up"></i></button>
+    </div>
 [{/if}]
 <script type="text/javascript" src="[{$oViewConf->getModuleUrl("_dev-core","out/libs/jquery/dist/jquery.min.js")}]"></script>
 <script type="text/javascript" src="[{$oViewConf->getModuleUrl("_dev-core","out/libs/velocity/velocity.min.js")}]"></script>
@@ -20,21 +20,31 @@
 [{ oxscript }]
 
 <script>
-    var app = angular.module('devApp', [ [{$smarty.capture.appdep}] ] )
-        .filter("html", ['$sce', function ($sce) { return function(htmlCode) { return $sce.trustAsHtml(htmlCode); } }])
+var app = angular.module('devApp', [ [{$smarty.capture.appdep}] ])
+        .filter("html", ['$sce', function ($sce) {
+            return function (htmlCode) {
+                return $sce.trustAsHtml(htmlCode);
+            }
+        }])
         .filter("highlight", function () {
-            return function($value, $param) {
-                return $param ? $value.replace( new RegExp($param, "ig"), function swag(x){ return '<b class="hl">'+x+'</b>';}) : $value;
+            return function ($value, $param) {
+                return $param ? $value.replace(new RegExp($param, "ig"), function swag(x) {
+                    return '<b class="hl">' + x + '</b>';
+                }) : $value;
                 //return $param ? $value.split($param).join('<b class="hl">'+$param+'</b>') : $value;
-            } 
+            }
         })
-        .controller('devCtrl', function($scope, $http, LxDialogService, LxNotificationService) {
+        .config(['ngClipProvider', function (ngClipProvider) {
+            ngClipProvider.setPath('[{$oViewConf->getModuleUrl("_dev-core","out/libs/zeroclipboard/dist/ZeroClipboard.swf")}]');
+        }])
+        .controller('devCtrl', function ($scope, $http, LxDialogService, LxNotificationService) {
             [{$ng}]
 
-            $scope.scrolltotop = function() { $("body").animate({scrollTop: 0}, "slow"); };
+            $scope.scrolltotop = function () {
+                $("body").animate({scrollTop: 0}, "slow");
+            };
         });
 </script>
-
 
 </body>
 </html>
--- dev-logs/application/controllers/admin/vtdev_logs.php
+++ dev-logs/application/controllers/admin/vtdev_logs.php
@@ -29,7 +29,7 @@
  */
 class vtdev_logs extends oxAdminView
 {
-    protected $_sThisTemplate  = 'vt_dev_logs.tpl';
+    protected $_sThisTemplate = 'vt_dev_logs.tpl';
 
     protected $_sExLog = null;
     protected $_sErrLog = null;
@@ -57,7 +57,7 @@ class vtdev_logs extends oxAdminView
         //$this->addTplParam('exLog', $this->getExceptionLog());
         $this->addTplParam('errlog', $this->_sErrLog);
         $this->addTplParam('ip', $_SERVER['REMOTE_ADDR']);
-        
+
         //var_dump("<h2>".$this->_sExLogPath."</h2>");
         //$this->getExceptionLog();
         //echo "<hr/>";
@@ -75,134 +75,115 @@ class vtdev_logs extends oxAdminView
 
     public function getExceptionLog()
     {
-        $cfg = oxRegistry::getConfig();
+        $cfg    = oxRegistry::getConfig();
         $sExLog = $cfg->getConfigParam('sShopDir') . 'log/EXCEPTION_LOG.txt';
 
-        if (!file_exists($sExLog) || !is_readable($sExLog))
-        {
-            header('HTTP/1.1 500 Error reading ');
-            header('Content-Type: application/json; charset=UTF-8');
-            die(json_encode(array('error' => "EXCEPTION_LOG.txt does not exist or is not readable")));
+        if (!file_exists($sExLog) || !is_readable($sExLog)) die(json_encode(['status' => "EXCEPTION_LOG.txt does not exist or is not readable"]));
+
+        $sData = file_get_contents($sExLog);
+
+        $aData = explode("---------------------------------------------", $sData);
+        $aData = array_slice($aData, -101);
+        array_pop($aData); // cut last empty array element
+        foreach ($aData as $key => $value) {
+            $aEx     = explode("Stack Trace:", trim($value));
+            $aHeader = explode("[0]:", $aEx[0]);
+
+            $iFC = preg_match("/Faulty\scomponent\s\-\-\>\s(.*)/", $aEx[1], $aFC);
+
+            $aData[$key] = (object)array(
+                "header" => str_replace($cfg->getConfigParam("sShopDir"), "", trim($aHeader[0])),
+                "subheader" => str_replace($cfg->getConfigParam("sShopDir"), "", trim($aHeader[1])) . ($iFC == 1 ? ": " . $aFC[1] : ""),
+                "text" => htmlentities(str_replace($cfg->getConfigParam("sShopDir"), "", trim($aEx[1]))),
+                "full" => $value
+            );
         }
 
-        if ($sData = file_get_contents($sExLog))
-        {
-            $aData = explode("---------------------------------------------", $sData);
-            $aData = array_slice($aData, -101);
-            array_pop($aData); // cut last empty array element
-            foreach($aData as $key => $value)
-            {
-                $aEx = explode("Stack Trace:", trim($value));
-                $aHeader = explode("[0]:",$aEx[0]);
-                
-                $iFC = preg_match("/Faulty\scomponent\s\-\-\>\s(.*)/", $aEx[1], $aFC);
-                
-                $aData[$key] = (object)array(
-                    "header" => str_replace($cfg->getConfigParam("sShopDir"),"",trim($aHeader[0])),
-                    "subheader" => str_replace($cfg->getConfigParam("sShopDir"),"",trim($aHeader[1])). ($iFC == 1 ? ": ".$aFC[1] : ""),
-                    "text" => htmlentities(str_replace($cfg->getConfigParam("sShopDir"),"",trim($aEx[1]))),
-                    "full" => $value
-                );
-            }
-            
-            $time = filemtime($sExLog);
-            echo json_encode(array_reverse($aData));
-            exit;
-        }
-
-            header('HTTP/1.1 500 Error reading ');
-            header('Content-Type: application/json; charset=UTF-8');
-            die(json_encode(array('error' => "something went wrong")));
+        //$time = filemtime($sExLog);
+        die(json_encode(['status' => 'ok', 'log' => array_reverse($aData)]));
     }
 
     public function getErrorLog()
     {
         if (!$this->_sErrLog) return false;
-        
-        $cfg = oxRegistry::getConfig();
-        if (!file_exists($this->_sErrLog) || !is_readable($this->_sErrLog))
-        {
-            $this->addTplParam("error", (object)array("type" => "warning", "text" => "file does not exist or is not readable"));
 
-            header('HTTP/1.1 500 Error reading ');
-            header('Content-Type: application/json; charset=UTF-8');
-            die(json_encode(array('error' => "zhere is no error log")));
-        }
+        $cfg = oxRegistry::getConfig();
+        if (!file_exists($this->_sErrLog) || !is_readable($this->_sErrLog)) die(json_encode(['status' => "file does not exist or is not readable"]));
 
         $aData = file($this->_sErrLog);
 
         // xdebug?
-        if(function_exists('xdebug_get_code_coverage')) $this->_getXdebugErrorLog($aData);
+        if (function_exists('xdebug_get_code_coverage')) $this->_getXdebugErrorLog($aData);
 
         $aData = array_slice($aData, -300);
 
-        foreach($aData as $key => $value)
-        {
+        foreach ($aData as $key => $value) {
             /*
             [Sat May 30 10:32:38 2015] [error] [client 79.222.227.99] 
             PHP Fatal error:  Smarty error: [in vt_dev_footer.tpl line 7]: syntax error: unrecognized tag: $module_components:default:"'lumx'" (Smarty_Compiler.class.php, line 446) in /srv/ox/bla/core/smarty/Smarty.class.php on line 1093, referer: http://ox.marat.ws/bla/admin/index.php?editlanguage=0&force_admin_sid=5fdleoj00epaoe5d75d5hi6q01&stoken=C8D0F139&&cl=navigation&item=home.tpl
             */
-            preg_match_all("/\[([^\]]*)\]/",$value, $header);
-            $msg = trim(str_replace(array_slice($header[0],0,3),'',$value));
+
+            print_r($key);
+            print_r($value);
+
+            preg_match_all("/\[([^\]]*)\]/", $value, $header);
+            $msg = trim(str_replace(array_slice($header[0], 0, 3), '', $value));
 
             // in: between " in" and " referer"
-            preg_match("/\sin\s\/(.*)\sreferer\:/",$msg, $in);
-            
-            // referer: after "referer"
-            preg_match("/\sreferer\:(.*)/",$msg, $ref);
-            
-            $replace = [$ref[0],' in /'.$in[1]];
+            preg_match("/\sin\s\/(.*)\sreferer\:/", $msg, $in);
 
-            $aErr = array(
-                "date"   => date_format(date_create($header[1][0]),'Y-m-d H:i:s'),
-                "type"   => $header[1][1],
+            // referer: after "referer"
+            preg_match("/\sreferer\:(.*)/", $msg, $ref);
+
+            $replace = [$ref[0], ' in /' . $in[1]];
+
+            $aErr        = array(
+                "date" => date_format(date_create($header[1][0]), 'Y-m-d H:i:s'),
+                "type" => $header[1][1],
                 "client" => $header[1][2],
-                "header" => str_replace($replace,"",$msg),
-                "in" => str_replace($cfg->getConfigParam("sShopDir"),"","/".$in[1]),
+                "header" => str_replace($replace, "", $msg),
+                "in" => str_replace($cfg->getConfigParam("sShopDir"), "", "/" . $in[1]),
                 "referer" => $ref[1],
                 "full" => $value
-                
+
             );
-            $aData[$key] = (object) $aErr;
-            
+            $aData[$key] = (object)$aErr;
+
             // "in" => str_replace($cfg->getConfigParam("sShopDir"),"",substr($msg, strpos($msg, " in /")+3, strpos($msg, ", referer: "))),
         }
 
-        echo json_encode(array_reverse($aData));
+        echo json_encode(['status' => 'ok', 'log' => array_reverse($aData)]);
         exit;
     }
 
     protected function _getXdebugErrorLog($aData)
     {
-        $cfg = oxRegistry::getConfig();
+        $cfg  = oxRegistry::getConfig();
         $aLog = [];
 
-        $i = -1;
+        $i       = -1;
         $logdate = '';
-        foreach($aData as $row)
-        {
-            if($i>30) break; // 30 lag entries are enough, i suppose
-            preg_match("/\[([^\]]*)\]/",$row,$date);
+        foreach ($aData as $row) {
+            if ($i > 30) break; // 30 lag entries are enough, i suppose
+            preg_match("/\[([^\]]*)\]/", $row, $date);
 
             // stack trace
-            if((preg_match("/\] PHP Stack trace/",$row) > 0 || preg_match("/\] PHP\s+\d/",$row) > 0) && $date[1] == $logdate)
-            {
-                $aLog[$i]['stacktrace'][] = substr($row,strlen($date[0]));
+            if ((preg_match("/\] PHP Stack trace/", $row) > 0 || preg_match("/\] PHP\s+\d/", $row) > 0) && $date[1] == $logdate) {
+                $aLog[$i]['stacktrace'][] = substr($row, strlen($date[0]));
 
-            }
-            else // first log line
+            } else // first log line
             {
                 $i++;
                 $logdate = $date[1];
-                preg_match("/\] PHP\s(.*):/",$row,$type);
-                preg_match("/\sPHP.+\:\s+(.*)\sin\s/",$row,$header);
-                preg_match("/\sin\s\/(.*)/",$row, $in);
+                preg_match("/\] PHP\s(.*):/", $row, $type);
+                preg_match("/\sPHP.+\:\s+(.*)\sin\s/", $row, $header);
+                preg_match("/\sin\s\/(.*)/", $row, $in);
 
                 $aLog[$i] = [
-                    'date' => date_format(date_create($logdate),'Y-m-d H:i:s'),
+                    'date' => date_format(date_create($logdate), 'Y-m-d H:i:s'),
                     'type' => $type[1],
                     'header' => $header[1],
-                    'in' => str_replace($cfg->getConfigParam("sShopDir"),"","/".$in[1]),
+                    'in' => str_replace($cfg->getConfigParam("sShopDir"), "", "/" . $in[1]),
                     'stacktrace' => [],
                     'full' => $row
                 ];
